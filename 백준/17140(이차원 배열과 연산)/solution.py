c8c2186e34bb9d9f0d6bb3c32b1185225a54ac42
km-kwon <lanos5019@ajou.ac.kr>
2024-09-19 16:57:26 +0900

    Feat: 이차원

--- 백준/17140(이차원 배열과 연산)/solution.py
+++ 백준/17140(이차원 배열과 연산)/solution.py
@@ -1,3 +1,70 @@
+import copy
+def cal(row, col, arr):
+    temp = copy.deepcopy(arr)
+    maxLen = 0
+    result =[]
+    for i in range(row):
+        row_result = []
+        dic = {}
+        sort_temp  = []
+        for j in range(col):
+            if temp[i][j] == 0:
+                continue
+            if not temp[i][j] in dic:
+                dic[temp[i][j]] = [1,temp[i][j]]
+            else: 
+                dic[temp[i][j]][0] +=1
+        for j in dic.values():
+            sort_temp.append(j)
+        sort_temp = sorted(sort_temp,key=  lambda x:(x[0],x[1]))
+        for j in sort_temp:
+            row_result.append(j[1])
+            row_result.append(j[0])
+        maxLen = max(maxLen, len(row_result))
+        result.append(row_result)
+    for i in range(len(result)):
+        for j in range(maxLen-len(result[i])):
+            result[i].append(0)
+    return result
+
+
+def solution():
+    r, c, k = map(int,input().split())
+    r = r-1
+    c = c-1
+    time = 0
+    arr = []
+    for i in range(3):
+        arr.append(list(map(int,input().split())))
+    while True:
+        if arr[r][c] == k:
+            print(time)
+            break
+        arr = cal(len(arr), len(arr[0]), arr)
+        time+=1
+        if time > 100:
+            print(-1)
+            break
+        if arr[r][c] == k:
+            print(time)
+            break
+        arr = list(zip(*arr))
+        arr = cal(len(arr), len(arr[0]), arr)
+        arr = list(zip(*arr))
+        time+=1
+        if time > 100:
+            print(-1)
+            break
+        if arr[r][c]==k:
+            print(time)
+            break
+    return
+
+solution()
+
+
+
+
 """ import copy
 
 maxLen = 0
