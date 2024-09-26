9d29e140d073a9fd72993d7928283e89b81f4b5f
kyoungmin <lanos5019@ajou.ac.kr>
2024-09-26 23:19:51 +0900

    feat: 스타트와 링크 (시간복잡도 해결 예정)

--- 백준/14889(스타트와 링크)/solution.py
+++ 백준/14889(스타트와 링크)/solution.py
@@ -2,14 +2,16 @@ n = int(input())
 
 arr = []
 for i in range(n):
-    arr.append(list(map(int,input().split())))
+    arr.append(list(map(int, input().split())))
 
 team = [-1]*n
 
-minValue = 0
+minValue = 1e9
 
-def sol(start,n,team):
+
+def sol(start, n, team):
     global arr
+    global minValue
     if not -1 in team:
         aval1 = 0
         aval2 = 0
@@ -20,16 +22,21 @@ def sol(start,n,team):
                 start.append(i)
                 for j in start:
                     aval1 += (arr[j-1][i-1] + arr[i-1][j-1])
-            elif team[i] ==2:
+            elif team[i] == 2:
                 link.append(i)
-                for j in start:
+                for j in link:
                     aval2 += (arr[j-1][i-1] + arr[i-1][j-1])
-        
-    for i in range(start,n):
+        minValue = min(minValue, abs(aval1-aval2))
+        return
+    for i in range(start, n):
         team[i] = 1
-        sol(i+1,n,team)
+        sol(i+1, n, team)
         team[i] = 2
-        sol(i+1,n,team)
+        sol(i+1, n, team)
+        team[i] = -1
     return
 
-sol(0,n,team)
+
+sol(0, n, team)
+
+print(minValue)
