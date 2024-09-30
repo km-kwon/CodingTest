09b6e1767eea62388a73d56aff5a5d456839549f
km-kwon <lanos5019@ajou.ac.kr>
2024-09-30 15:29:14 +0900

    Feat:낚시왕

--- 백준/17143(낚시왕)/solution.py
+++ 백준/17143(낚시왕)/solution.py
@@ -6,21 +6,48 @@ dy = [-1, 1, 0 ,0]
 count = 0
 
 R,C,m = map(int,input().split())
-arr = [[0] * C for _ in range(R)]
 shark = {}
 
 for i in range(m):
     r,c,s,d,z = map(int,input().split())
-    arr[r][c] = (z,s,d-1)
+    r = r-1
+    c = c-1
     shark[(r,c)] = (z,s,d-1)
     # z: 사이즈 s: 속력 d-1 : 방향
 
-cur = -1
-for i in range(C+1):
-    cur += 1
+for cur in range(C):
     for j in range(R):
-        if (i,j) in shark:
-            count += shark[(i,j)][0]
-            del shark[(i,j)]
-for i,j in shark.keys:
-    
+        if (j,cur) in shark:
+            count += shark[(j,cur)][0]
+            del shark[(j,cur)]
+            break
+    temp = []
+    for i,j in shark.keys():
+        curx = j
+        cury = i 
+        curZ, curSpeed, curDir = shark[(i,j)]
+        for k in range(curSpeed):
+            if curx + dx[curDir]<0 or curx + dx[curDir]>=C or cury + dy[curDir] < 0 or cury + dy[curDir]>=R:
+                if curDir == 0:
+                    curDir = 1
+                elif curDir == 1:
+                    curDir = 0
+                elif curDir == 2:
+                    curDir = 3
+                elif curDir == 3:
+                    curDir = 2
+            curx = curx + dx[curDir]
+            cury = cury + dy[curDir]
+        temp.append((cury,curx,curZ, curSpeed, curDir))
+    afterShark = {}
+    for y,x,z,speed,dir in temp:
+        if not (y,x) in afterShark:
+            afterShark[(y,x)] = (z,speed,dir)
+        else:
+            if afterShark[(y,x)][0] > z:
+                continue
+            else:
+                afterShark[(y,x)] = (z,speed,dir)
+    shark = afterShark
+
+print(count)
