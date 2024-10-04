b3fad36b7a6256552bca03a421daab2064715678
km-kwon <lanos5019@ajou.ac.kr>
2024-10-04 16:53:26 +0900

    feat: 새로운게임2

--- 백준/17837(새로운 게임2)/solution.py
+++ 백준/17837(새로운 게임2)/solution.py
@@ -8,33 +8,26 @@ arr = []
 # 현재 말의 위치
 loc = [0]*K
 # 판의 상태
-status = [[deque()]*N for _ in range(N)]
+status = [[deque() for _ in range(N)] for _ in range(N)]
 
 for i in range(N):
     arr.append(list(map(int,input().split())))
 
 for i in range(K):
     r,c,dir = map(int,input().split())
-    loc[i] = (r,c,dir-1)
-    status[r][c].append(i)
+    loc[i] = [r-1,c-1,dir-1]
+    status[r-1][c-1].append(i)
 
-turn = 0
-while True:
+turn = 1
+while turn<=1000:
     for i in range(K):
         cury = loc[i][0]
         curx = loc[i][1]
         curdir = loc[i][2]
-
-        move = []
-        flag = 0
-        for j in range(status[cury][curx]):
-            if j == i:
-                flag = 1
-            if flag == 1: 
-                move.append(j)
-
+        
         ny = cury + dy[curdir]
         nx = curx + dx[curdir]
+        # 일단 파란색인지 영역밖인지 확인
         if ny < 0 or ny >=N or nx <0 or nx >= N or arr[ny][nx] == 2:
             if curdir == 0:
                 curdir = 1
@@ -44,5 +37,46 @@ while True:
                 curdir = 3
             elif curdir == 3:
                 curdir = 2 
-        else:
-            print()
+            ny = cury + dy[curdir]
+            nx = curx + dx[curdir]
+            loc[i][2] = curdir
+        if ny>=0 and ny < N and nx >=0 and nx<N and arr[ny][nx] != 2:
+            move = deque()
+            resultMove = deque()
+            flag = 0
+            for j in status[cury][curx]:
+                if j == i:
+                    flag = 1
+                    move.append(j)
+                    continue
+                if flag == 1: 
+                    move.append(j)
+                else:
+                    resultMove.append(j)
+            status[cury][curx] = resultMove
+            if arr[ny][nx] == 0:
+                for j in move:
+                    status[ny][nx].append(j)
+                    loc[j][0] = ny
+                    loc[j][1] = nx
+            elif arr[ny][nx] == 1:
+                for j in range((-len(move) + 1), 1):
+                    status[ny][nx].append(move[-j])
+                    loc[move[-j]][0] = ny
+                    loc[move[-j]][1] = nx
+    tempR = loc[0][0]
+    tempC = loc[0][1]
+    checkFlag = True  
+
+    for j in range(1,len(loc)):
+        if loc[j][0] != tempR or loc[j][1] != tempC:
+            checkFlag = False
+            break
+    if checkFlag:
+        break
+    turn += 1
+
+if turn > 1000:
+    print(-1)
+else:
+    print(turn)
