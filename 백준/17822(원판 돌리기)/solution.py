3e8e3b6448b219b33df5dd0ae3aee4ee28d204ad
kyoungmin <lanos5019@ajou.ac.kr>
2024-10-09 23:06:15 +0900

    feat: 원판 돌리기

--- 백준/17822(원판 돌리기)/solution.py
+++ 백준/17822(원판 돌리기)/solution.py
@@ -1,15 +1,15 @@
 from collections import deque
 
-N,M,T = map(int,input().split())
+N, M, T = map(int, input().split())
 status = [deque() for _ in range(N+1)]
 
 for i in range(1, N+1):
-    temp = list(map(int,input().split()))
+    temp = list(map(int, input().split()))
     for j in temp:
         status[i].append()
 
 for i in range(T):
-    x,d,k = map(int,input().split())
+    x, d, k = map(int, input().split())
     # 돌려돌려 돌림판
     for j in range(1, N):
         # j가 x의 배수라면 움직여야하는 원판
@@ -26,3 +26,10 @@ for i in range(T):
     # 전체 순회 및 체크
     for j in range(1, len(status)+1):
         for num in range(M):
+            if sum(status[j]) != -M:
+                # 이거 약간 같은게 연속해서 주루룩 있으면 그거 해결해야할듯
+                if status[j][num] == -1:
+                    continue
+                elif status[j][num] == status[j][num-1]:
+                    change = True
+                    status[j][num-1]
